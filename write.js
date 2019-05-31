7a3a0e3627788ec8c387580f969c256af9c32c55
unknown <workdennis@yahoo.com>
2019-05-30 20:19:29 -0500

    lkjlj

---
+++ write.js
@@ -0,0 +1,43 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "codingtodo_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  ask();
+});
+
+function ask(){
+  inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+       type: "input",
+      message: "What's your next idea?",
+      name: "new"
+    }
+    ]).then(function(inquirerResponse) {
+      var inserted = inquirerResponse.new;
+      insertion(inserted);
+    })
+}
+
+function insertion(inserted) {
+  connection.query("INSERT INTO todolist (task) VALUES ('" + addition + "');", function(err, res) {
+    if (err) throw err;
+    
+    // console.log(res[0].task);
+    connection.end();
+  });
+}
